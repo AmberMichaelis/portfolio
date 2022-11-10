3e474931b8753b461b781a0b61949ffccafd8aa2
Amber M <amberswieczkowski@pm.me>
2022-11-09 22:39:04 -0600

    Add resume info to resume page. Still needs mobile responsive css.

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -5,42 +5,240 @@ import './Resume.css';
 import '../index.css';
 import spaceVideo from '../assests/space.mp4';
 
-import { Link } from 'react-router-dom';
-
 const Resume = () => {
   return (
     <>
       <video autoPlay loop muted id='componentVideo'>
         <source src={spaceVideo} type='video/mp4' />
       </video>
-      <div className='resume'>
+      <div className='resume resume__body'>
         <div className='resume-left'>
-          <h1>Resume</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
-            expedita consectetur voluptates hic adipisci doloribus nostrum
-            quaerat nam ducimus magnam! Expedita, ex rem suscipit iusto corporis
-            soluta hic consectetur cupiditate.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ab magnam eos amet praesentium! Totam, fugit laudantium commodi vel, quos accusamus numquam, ipsa quo laboriosam harum iure reiciendis illo magnam.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit assumenda dicta rerum eaque possimus odio provident esse optio, amet asperiores molestiae alias labore deserunt debitis voluptatum perferendis aut. Esse, eaque.
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, ipsa corrupti. Officia, quasi tempore quibusdam tempora, ducimus aliquam odit asperiores accusamus quia repudiandae alias nemo harum officiis aliquid ratione quas?
-          </p>
-          <Link to='/contact'>
-            <button className='btn'>Contact</button>
-          </Link>
+
+        <div className="profile__text">
+            <h2>Amber Michaelis <br /><span>Full Stack Developer</span></h2>
+            <div className="resume__contact__info resume__projects flip">
+              <a
+                href="https://www.krustykrab.us/projects.html"
+                target="_blank"
+                rel="noreferrer"
+                className="resume__projects__button"
+                data-back="Click Me"
+                data-front="Personal Projects"
+              >
+              </a>
+            </div>
+          </div>
+
+          <div className="resume__contact__info">
+            <h3 className="resume__contact__title">Contact Info</h3>
+            <ul>
+              <a
+                href="mailto:amberm31@protonmail.com?subject=Development Inquiry"
+                target="_blank"
+                rel="noreferrer"
+                className="resume__contact__link"
+              >
+                <li>
+                  <span className="resume__contact__text"
+                    >AmberM31@protonmail.com</span
+                  >
+                </li>
+              </a>
+              <a
+                href="https://nimble-truffle-a9d6f9.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+                className="resume__contact__link"
+              >
+                <li>
+                  <span className="resume__contact__text"
+                    >MyWeddingWebsite.online (React)</span
+                  >
+                </li>
+              </a>
+              <a
+                href="https://www.krustykrab.us/"
+                target="_blank"
+                rel="noreferrer"
+                className="resume__contact__link"
+              >
+                <li>
+                  <span className="resume__contact__text"
+                    >KrustyKrab.us (JavaScript)</span
+                  >
+                </li>
+              </a>
+              <a
+                href="https://github.com/AmberMichaelis"
+                target="_blank"
+                rel="noreferrer"
+                className="resume__contact__link"
+              >
+                <li>
+                  <span className="resume__contact__text"
+                    >GitHub.com/AmberMichaelis</span
+                  >
+                </li>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/ambermichaelis/"
+                target="_blank"
+                rel="noreferrer"
+                className="resume__contact__link"
+              >
+                <li>
+                  <span className="resume__contact__text"
+                    >LinkedIn.com/AmberMichaelis</span
+                  >
+                </li>
+              </a>
+              <li>
+                <span className="resume__contact__text">Remote, USA</span>
+              </li>
+            </ul>
+          </div>
+
+          <div className="resume__contact__info resume__education">
+            <h3 className="resume__contact__title">Education</h3>
+            <ul>
+              <li>
+                <h5>2022 - Present</h5>
+                <h4>MS Computer Science</h4>
+                <h6>
+                  Data Sturctures & Algorithms <br />
+                  Professional Certificate
+                </h6>
+                <h4>Georgia Institute of Technology (USA)</h4>
+              </li>
+              <li>
+                <h5>2012 - 2017</h5>
+                <h4>BS Chemical Engineering</h4>
+                <h6>Minors: Math, Biology, Chemistry</h6>
+                <h4>McNeese State University (USA)</h4>
+              </li>
+            </ul>
+          </div>
         </div>
         <div className='resume-right'>
           <div className='rightStuff'>
-            <h1>Experience</h1>
+
+          <div className="resume__about">
+            <h2 className="resume__about__title">Profile</h2>
             <p>
-              rightStuff goes here. Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Vel voluptatem explicabo excepturi eos eaque
-              consequatur in. Unde possimus itaque corrupti voluptates voluptas
-              enim inventore deleniti non adipisci ut, natus repellat.
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero ad repellendus enim ut laborum possimus quaerat, architecto eum culpa accusantium velit consequatur reprehenderit consequuntur nam ipsam tempore labore excepturi voluptatum!
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum sunt possimus enim unde earum minima officiis laudantium facilis? Adipisci perferendis earum dolor voluptatum quae ipsam veniam dolorum. Voluptatem, nostrum alias.
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem nam rem quam! Nesciunt ab rem odit, nulla rerum quo facilis aperiam pariatur et! Illo, voluptate aperiam. Nostrum itaque optio hic.
+              Software Developer with 2 years of full stack development
+              experience. 4 years of experience working remotely. Experience
+              with unit testing using Jest, libraries including jQuery, database
+              tools such as Postman, and asynchronous programming. Scored in the
+              top 5% of 2.2M people on LinkedIn JavaScript assessment.
+              <br /><br />Pursuing an online Masters of Computer Science. Enjoy
+              being in nature and reading.
             </p>
+          </div>
+          <div className="resume__about">
+            <h2 className="resume__about__title">Experience</h2>
+            <div className="resume__experience__box">
+              <div className="resume__experience__year__company">
+                <h5>2021 - Present</h5>
+                <h5>Starbase Security<br/>Solutions</h5>
+                <h5 className="resume__experience__location">Remote USA/Europe</h5>
+              </div>
+              <div className="resume__experience__text">
+                <h4>Lead Full Stack SERN Developer</h4>
+                <p>
+                  Team lead using SQL, Express, React, Node. Helping your rockets get to the right planet the first time. Giving you more than just security, giving you peace of mind.
+                </p>
+              </div>
+            </div>
+            <div className="resume__experience__box">
+              <div className="resume__experience__year__company">
+                <h5>2022 - Present</h5>
+                <h5>Self-Employed</h5>
+                <h5 className="resume__experience__location">Remote USA/Europe</h5>
+              </div>
+              <div className="resume__experience__text">
+                <h4>Freelance Contractor</h4>
+                <p>
+                  Various frontend and backend mostly in JS and Node.
+                  Refactoring code and writing tests for decentralized
+                  storage platform. Maintaining multiple non-profit websites.
+                </p>
+              </div>
+            </div>
+            <div className="resume__experience__box">
+              <div className="resume__experience__year__company">
+                <h5>2018 - 2021</h5>
+                <h5>Lumberton & Bryan High School</h5>
+                <h5 className="resume__experience__location">Remote/Texas, USA</h5>
+              </div>
+              <div className="resume__experience__text">
+                <h4>Engineering, Physics, Math Instructor</h4>
+                <p>
+                  Experience working remotely. Introduction into programming
+                  with Python and Scratch. Calculus-Based AP Physics, Math,
+                  Chemistry, and Finance.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="resume__about resume__skills">
+            <h2 className="resume__about__title">Professional Skills</h2>
+            <div className="resume__experience__box">
+              <h4>
+                <i className="fab fa-js"></i>
+                JavaScript
+              </h4>
+              <div className="resume__languages__percent">
+              <div style={{width: "100%"}}></div>
+              </div>
+            </div>
+            <div className="resume__experience__box">
+              <h4>
+                <i className="fab fa-react"></i>
+                React
+              </h4>
+              <div className="resume__languages__percent">
+              <div style={{width: "85%"}}></div>
+              </div>
+            </div>
+            <div className="resume__experience__box">
+              <h4>
+                <i className="fab fa-node-js"></i>
+                Node
+              </h4>
+              <div className="resume__languages__percent">
+              <div style={{width: "95%"}}></div>
+              </div>
+            </div>
+            <div className="resume__experience__box">
+              <h4>
+                <i className="fab fa-java"></i>
+                Java
+              </h4>
+              <div className="resume__languages__percent">
+              <div style={{width: "80%"}}></div>
+              </div>
+            </div>
+            <div className="resume__experience__box">
+              <h4>
+                <i className="fab fa-git-alt"></i>
+                Git
+              </h4>
+              <div className="resume__languages__percent">
+              <div style={{width: "95%"}}></div>
+              </div>
+            </div>
+            <div className="resume__experience__box">
+              <h4>
+                <i className="fas fa-database"></i>
+                MySQL
+              </h4>
+              <div className="resume__languages__percent">
+              <div style={{width: "100%"}}></div>
+              </div>
+            </div>
+            </div>
+
           </div>
         </div>
       </div>
